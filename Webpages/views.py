05ac00e26344ebb8da40cb48cc2c778ef4e8a542
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-21 23:05:46 +0530

    Adding Notes for Users

--- Webpages/views.py
+++ Webpages/views.py
@@ -4,9 +4,12 @@ This file has the routes of all the pages where the
 user would be going
 '''
 
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template , flash , request
 
 from flask_login import login_required , current_user
+
+from .models import Note
+from . import db
 
 views = Blueprint('views' , __name__);
 
@@ -14,6 +17,17 @@ views = Blueprint('views' , __name__);
 @views.route('/' , methods = ["GET" , "POST"])
 @login_required
 def home():
+    if request.method == "POST":
+        note = request.form.get("note");
+
+        if len(note) < 1:
+            flash("Note is too short to be noted!" , category="error")
+        else :
+            new_note = Note(data = note , user_id = current_user.id);
+            db.session.add(new_note);
+            db.session.commit();
+            flash("Note Added!" , category="success")
+
     return render_template("home.html" , user = current_user);
 
 
