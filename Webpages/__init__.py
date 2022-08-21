932d25b530eff52d36957456f88f8622741de5d9
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-21 16:06:28 +0530

    Initializing Database in __init__

--- Webpages/__init__.py
+++ Webpages/__init__.py
@@ -10,10 +10,16 @@ from Webpages import initiate_app()
 '''
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy();
+DB_NAME = database.db;
 
 def initiate_app():
     app = Flask(__name__);
     app.config['SECRET_KEY'] = "ifyoureadthisyoushouldn'tbehere"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app);
 
     from .views import views
     from .auth import auth
